dcb351105a8ac306df8c5fb50b9bc1937fa60d20
Ciro Poma <ciro.poma@vallegrande.edu.pe>
2026-06-10 09:12:09 -0500

    feat: add spring boot microservices

---
+++ microservices/tour-package-service/src/main/java/com/angoma/tour_package_service/TourPackageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.angoma.tour_package_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourPackageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourPackageServiceApplication.class, args);
+	}
+
+}
---
+++ microservices/tour-package-service/src/test/java/com/angoma/tour_package_service/TourPackageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angoma.tour_package_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourPackageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
